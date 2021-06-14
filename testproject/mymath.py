be5034954d3508c840aea6c2d7fe98bfd08b94f8
Garay Krisztina <64037359+otedam@users.noreply.github.com>
2021-06-14 21:23:09 +0200

    Create mymath.py

---
+++ testproject/mymath.py
@@ -0,0 +1,20 @@
+def add_numbers(a, b):
+    return a + b
+tartalommal, 
+4. létrehozod szintén abban a mappában a test_mymath.py file-t
+import pytest
+
+from mymath import add_numbers
+
+def test_add_positive():
+    assert add_numbers(1, 2) == 3
+
+def test_add_zero():
+    assert add_numbers(1, 0) == 1
+
+def test_add_negative():
+    assert add_numbers(4, -100) == -96
+
+def test_add_string__expect_exception():
+    with pytest.raises(TypeError):
+        add_numbers(4, 'I DO NOT BELONG HERE')
