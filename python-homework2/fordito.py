8a0f70e302a76a8efebecb8c237bdbe7cdb0aa55
Garay Krisztina <edward7ote@gmail.com>
2021-06-28 21:55:55 +0200

    fordito.py modified hf added

--- python-homework2/fordito.py
+++ python-homework2/fordito.py
@@ -3,19 +3,49 @@
 # majd írja ki a képernyőre a lista elemeit fordított sorrendben!
 # A megoldást egy fordito.py nevű file-ban kell beadnod.
 
+# lista = []
+# valt = True
+#
+# try:
+#     while valt:
+#         try:
+#             beker = int(input("Kérek egy pozitív számot: "))
+#         except:
+#             print("Pozitív számot kértem!")
+#         if beker == 0:
+#             valt = False
+#             break
+#         elif beker < 0:
+#             print("pozitív számot kértem")
+#         elif beker > 0:
+#             lista.append(beker)
+#             print(lista)
+#
+#     lista.reverse()
+#     print(lista)
+#
+# except:
+#     print("Hiba vhol...")
+
 lista = []
 valt = True
+
 while valt:
-    beker = int(input("Kérek egy pozitív számot: "))
-    if beker == 0:
-        valt = False
-        break
-    elif beker < 0:
-         print("pozitív számot kértem")
-    elif beker > 0:
-         lista.append(beker)
-         print(lista)
+    beker = input("Kérek egy pozitív számot: ")
+    if beker.isalpha():
+        print("Pozitív számot kértem bent!")
+        continue
     else:
-        print("Pozitív számot kértem!")
+        beker = int(beker)
+        if beker == 0:
+            valt = False
+            break
+        elif beker < 0:
+            print("pozitív számot kértem")
+        elif beker > 0:
+            lista.append(beker)
+            print(lista)
+# print("Pozitív számot kértem!")
+
 lista.reverse()
 print(lista)
