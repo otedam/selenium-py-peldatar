fa0e94fe1a3a241352adc950fd29aaa5cd7289d0
Garay Krisztina <edward7ote@gmail.com>
2021-06-30 18:00:17 +0200

    fajl2 modified

--- python2-homework/fajl2.py
+++ python2-homework/fajl2.py
@@ -10,6 +10,7 @@
 #         my_list.append(queue)
 # print(my_list)
 
+####   Soronként beolvasva és összefűzve:
 with open('adat.txt', 'r') as f2:
     # Read and print the entire file line by line
     line = f2.readline()
@@ -18,5 +19,12 @@ with open('adat.txt', 'r') as f2:
         my_list.append(line)
         line = f2.readline()
     print(my_list, end='')
-print(my_list)
 
+print()
+
+####  Egyben beolvasva az össze sort, majd kiírva
+with open("adat.txt") as f2:
+    newf2 = f2.readlines()
+    print(newf2)
+
+#
